bc976dbd2b88f9bb884aca8771334a8e134f550f
jinyeop <bb3598@naver.com>
2022-08-10 16:20:30 +0900

    useEffect fetch

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,12 @@
-import NavBar from "../components/NavBar";
+import Head from "next/head";
+import Seo from "../components/Seo";
 
 export default function Potato() {
 	// return "about us";
+
 	return (
 		<div>
+			<Seo title="about" />
 			<h1>about</h1>
 		</div>
 	);
